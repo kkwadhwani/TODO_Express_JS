67611849690fd2e32e22239c3ac29b3c1d893e7e
Kunal Wadhwani <kkwadhwani@gmail.com>
2022-01-15 22:56:50 +0530

    few changes

--- app.js
+++ app.js
@@ -1,46 +1,36 @@
-const express= require('express');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
-const path = require('path');
-require('dotenv').config()
-const homeRoute = require('./routes/route')
-var methodOverride = require('method-override')
+const mongoose = require("mongoose");
+const path = require("path");
+require("dotenv").config();
+const homeRoute = require("./routes/route");
+var methodOverride = require("method-override");
 
 //method override
 
-app.use(methodOverride('_method'))
+app.use(methodOverride("_method"));
 //body-parse
 
-app.use(express.json())
-app.use(express.urlencoded())
-
-
+app.use(express.json());
+app.use(express.urlencoded());
 
 //DB setup
 
-mongoose.connect(process.env.DB_ACCESS)
-const connection = mongoose.connection
+mongoose.connect(process.env.DB_ACCESS);
+const connection = mongoose.connection;
 
-
+connection.once("open", () => {
+  console.log("DB running OK");
+});
 
 //view engine set up
 
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
-
-
-
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 //routes
-app.use('/', homeRoute)
+app.use("/", homeRoute);
 
-
-connection.once('open', ()=>{
-    console.log("DB running OK")
-})
-
-
-
-app.listen(process.env.PORT||3000, ()=>{
-    console.log("Server running")
-})
+app.listen(process.env.PORT || 3000, () => {
+  console.log("Server running");
+});
